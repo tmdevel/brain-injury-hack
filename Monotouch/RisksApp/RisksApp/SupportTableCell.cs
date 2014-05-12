c00691ee31eae62882d0a28fc18ed5689e06ea70
cemokeet <unknown-user@192.168.2.9>
2014-05-12 15:22:29 +0100

    Added content to the SupportView

---
+++ Monotouch/RisksApp/RisksApp/SupportTableCell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace RisksApp
+{
+	public partial class SupportTableCell : UITableViewCell
+	{
+		public static readonly UINib Nib = UINib.FromName ("SupportTableCell", NSBundle.MainBundle);
+		public static readonly NSString Key = new NSString ("SupportTableCell");
+
+		public SupportTableCell () : base ()
+		{
+
+		}
+
+		public SupportTableCell (IntPtr handle) : base (handle)
+		{
+
+		}
+
+		public static SupportTableCell Create ()
+		{
+			return (SupportTableCell)Nib.Instantiate (null, null) [0];
+		}
+
+		public void Bind(UIColor colour, String desc) {
+			ContentColourView.BackgroundColor = colour;
+			DescriptionLabel.Lines = 0;
+			DescriptionLabel.Text = desc;
+		}
+	}
+}
+
